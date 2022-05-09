19eadf19d74603de2159b6a8a37fd00175f3487b
neoaces <anzlemchavez@gmail.com>
2022-05-09 09:46:51 -0400

    Added post response

--- lib.go
+++ lib.go
@@ -3,13 +3,33 @@ package main
 import (
 	"net/http"
 
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
 var cards []string
 
+func getCard(ctx *gin.Context) {
+	cardID := ctx.Param("cardID")
+
+	if index, err := strconv.Atoi(cardID); err == nil {
+		ctx.JSON(http.StatusOK, &cards[index])
+	} else {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Wrong card ID"})
+
+	}
+}
+
 func returnCards(ctx *gin.Context) {
 	ctx.JSON(http.StatusAccepted, gin.H{"cards": cards})
+}
+
+func addCards(ctx *gin.Context) {
+	item := ctx.PostForm("item")
+	cards = append(cards, item)
+
+	ctx.String(http.StatusCreated, ctx.FullPath())
 }
 
 func main() {
@@ -21,6 +41,8 @@ func main() {
 	// })
 
 	r.GET("/", returnCards)
+	r.GET("/:cardID", getCard)
+	r.POST("/api", addCards)
 
 	r.Run()
 }
