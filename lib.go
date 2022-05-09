5c95f67d58c40a6be08ad3eab276f3c22ff3d8d9
neoaces <anzlemchavez@gmail.com>
2022-05-09 09:28:08 -0400

    return a regular array

--- lib.go
+++ lib.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
+var cards []string
+
+func returnCards(ctx *gin.Context) {
+	ctx.JSON(http.StatusAccepted, gin.H{"cards": cards})
+}
+
 func main() {
 	r := gin.Default()
+	cards = append(cards, "New Card")
 
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.String(200, "Welcome back.")
-	})
+	// r.GET("/", func(ctx *gin.Context) {
+	// 	ctx.String(200, "Welcome back.")
+	// })
+
+	r.GET("/", returnCards)
 
 	r.Run()
 }
